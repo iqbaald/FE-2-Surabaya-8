9c6f5a99e599406752da5987cad32e0011ba9ced
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-11-14 23:22:58 +0700

    Add Porfile Dropdown

--- js/script.js
+++ js/script.js
@@ -8,46 +8,52 @@ const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 
 // Show mobile menu
 hamburgerBtn.addEventListener("click", () => {
-    navbarMenu.classList.toggle("show-menu");
+  navbarMenu.classList.toggle("show-menu");
 });
 
 // Hide mobile menu
-hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+hideMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
 // Show login popup
 showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
+  document.body.classList.toggle("show-popup");
 });
 
 // Hide login popup
 hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
 
 // Show or hide signup form
-signupLoginLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
-    });
+signupLoginLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    formPopup.classList[link.id === "signup-link" ? "add" : "remove"](
+      "show-signup"
+    );
+  });
 });
 
 // Light Slider
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
 });
 
 // Light Slider 2
-$(document).ready(function() {
-    $('#autoWidthh').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidthh').removeClass('cS-hidden');
-        } 
-    });  
+$(document).ready(function () {
+  $("#autoWidthh").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidthh").removeClass("cS-hidden");
+    },
+  });
 });
+
+function newFunction() {
+  return "subMenu";
+}
