b62905cce4b28851d4055dab70dcafad962ac0e0
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2023-11-25 07:14:51 +0700

    Import dari hampir-main

---
+++ js/productbox_explore.js
@@ -0,0 +1,206 @@
+// Buat img-grid paket budaya
+const endpoint3 = "https://dummyjson.com/products";
+
+fetch(endpoint3)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (data && data.products) {
+      // data.products.forEach(product => {
+        for (let i = 0; i < Math.min(data.products.length, 4); i++){
+          const product = data.products[i];
+
+          // buat budaya box
+          var budaya = document.createElement("a");
+          budaya.classList.add("img-budaya");
+          budaya.setAttribute("href", "detail.html");
+
+          // buat isi image
+          var image = document.createElement("img");
+          image.setAttribute("src", product.thumbnail);
+
+          // buat isi text
+          var text = document.createElement("div");
+          text.classList.add("overlay");  
+          var isiText = document.createTextNode(product.title);;
+
+          // append 
+          budaya.appendChild(image);
+          budaya.appendChild(text);
+
+          text.appendChild(isiText)
+
+        
+        let paketBudaya = document.getElementById("img-grid");
+        paketBudaya.appendChild(budaya);
+      };
+    } else {
+      console.error("Data tidak sesuai", data);
+    }
+  })
+  .catch(error => {
+    console.log("Error", error);
+  });
+
+  // Buat product box paket international
+const endpoint2 = "https://dummyjson.com/products";
+
+fetch(endpoint2)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (data && data.products) {
+      data.products.forEach(product => {
+        // buat product-box
+        var productBox = document.createElement("div");
+        productBox.classList.add("product-box");
+
+        // buat img dan url
+        var img = document.createElement("img");
+        img.setAttribute("src", product.thumbnail);
+
+        // buat h2, p, span
+        var h2 = document.createElement("h2");
+        var title = document.createTextNode(product.title);
+
+        var p = document.createElement("p");
+        var harga = document.createTextNode("Harga mulai dari:");
+
+        var span = document.createElement("span");
+        var price = document.createTextNode("Rp. " + product.price);
+
+        // buat lokasi icon dan alamat
+        var location = document.createElement("div");
+        location.classList.add("location");
+
+        // buat isi div location
+        var div_icon = document.createElement("div");
+        var icon = document.createElement("img");
+        icon.classList.add("icon")
+        icon.setAttribute("src", "/Source/Img/Icon/location.png");
+
+        var div_alamat = document.createElement("div");
+        var alamat = document.createTextNode(product.brand);
+
+        // buat link untuk button cart
+        var cart = document.createElement("a");
+        cart.setAttribute("href","detail.html")
+        cart.classList.add("cart");
+
+        var cart_icon = document.createElement("img");
+        cart_icon.setAttribute("src", "/Source/Img/Icon/cart.png");
+
+        // append child
+        productBox.appendChild(img);
+        productBox.appendChild(h2);
+        productBox.appendChild(p);
+        productBox.appendChild(span);
+        productBox.appendChild(location);
+        productBox.appendChild(cart);
+
+        h2.appendChild(title);
+        p.appendChild(harga);
+        span.appendChild(price);
+        location.appendChild(div_icon)
+        div_icon.appendChild(icon)
+        div_icon.appendChild(alamat)
+        cart.appendChild(cart_icon)
+
+        let productContainer = document.getElementById("productContainer2");
+        productContainer.appendChild(productBox);
+      });
+    } else {
+      console.error("Data tidak sesuai", data);
+    }
+  })
+  .catch(error => {
+    console.log("Error", error);
+  });
+
+
+// Buat product box paket nasional
+const endpoint = "https://dummyjson.com/products";
+
+fetch(endpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (data && data.products) {
+      data.products.forEach(product => {
+        // buat product-box
+        var productBox = document.createElement("div");
+        productBox.classList.add("product-box");
+
+        // buat img dan url
+        var img = document.createElement("img");
+        img.setAttribute("src", product.thumbnail);
+
+        // buat h2, p, span
+        var h2 = document.createElement("h2");
+        var title = document.createTextNode(product.title);
+
+        var p = document.createElement("p");
+        var harga = document.createTextNode("Harga mulai dari:");
+
+        var span = document.createElement("span");
+        var price = document.createTextNode("Rp. " + product.price);
+
+        // buat lokasi icon dan alamat
+        var location = document.createElement("div");
+        location.classList.add("location");
+
+        // buat isi div location
+        var div_icon = document.createElement("div");
+        var icon = document.createElement("img");
+        icon.classList.add("icon")
+        icon.setAttribute("src", "/Source/Img/Icon/location.png");
+
+        var div_alamat = document.createElement("div");
+        var alamat = document.createTextNode(product.brand);
+
+        // buat link untuk button cart
+        var cart = document.createElement("a");
+        cart.setAttribute("href","detail.html")
+        cart.classList.add("cart");
+
+        var cart_icon = document.createElement("img");
+        cart_icon.setAttribute("src", "/Source/Img/Icon/cart.png");
+
+        // append child
+        productBox.appendChild(img);
+        productBox.appendChild(h2);
+        productBox.appendChild(p);
+        productBox.appendChild(span);
+        productBox.appendChild(location);
+        productBox.appendChild(cart);
+
+        h2.appendChild(title);
+        p.appendChild(harga);
+        span.appendChild(price);
+        location.appendChild(div_icon)
+        div_icon.appendChild(icon)
+        div_icon.appendChild(alamat)
+        cart.appendChild(cart_icon)
+
+        let productContainer = document.getElementById("productContainer");
+        productContainer.appendChild(productBox);
+      });
+    } else {
+      console.error("Data tidak sesuai", data);
+    }
+  })
+  .catch(error => {
+    console.log("Error", error);
+  });
--- js/script.js
+++ js/script.js
@@ -6,37 +6,85 @@ const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = formPopup.querySelector(".close-btn");
 const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 
+// profile menu
+let subUntukMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+  subUntukMenu.classList.toggle("open-menu");
+}
 // Show mobile menu
 hamburgerBtn.addEventListener("click", () => {
-    navbarMenu.classList.toggle("show-menu");
+  navbarMenu.classList.toggle("show-menu");
 });
 
 // Hide mobile menu
-hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+hideMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
 // Show login popup
 showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
+  document.body.classList.toggle("show-popup");
 });
 
 // Hide login popup
 hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
 
 // Show or hide signup form
-signupLoginLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
-    });
+signupLoginLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    formPopup.classList[link.id === "signup-link" ? "add" : "remove"](
+      "show-signup"
+    );
+  });
 });
 
 // Light Slider
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
 });
+
+// Light Slider 2
+$(document).ready(function () {
+  $("#autoWidthh").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidthh").removeClass("cS-hidden");
+    },
+  });
+});
+
+function newFunction() {
+  return "subMenu";
+}
+
+// Detail Page
+const imgs = document.querySelectorAll(".img-select a");
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+  imgItem.addEventListener("click", (event) => {
+    event.preventDefault();
+    imgId = imgItem.dataset.id;
+    slideImage();
+  });
+});
+
+function slideImage() {
+  const displayWidth = document.querySelector(
+    ".img-showcase img:first-child"
+  ).clientWidth;
+
+  document.querySelector(".img-showcase").style.transform = `translateX(${
+    -(imgId - 1) * displayWidth
+  }px)`;
+}
+
+window.addEventListener("resize", slideImage);
